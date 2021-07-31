04bdfc144c5b46447684c8c348226b3731ff24ff
Chris Brosnan <cbrosnan00@gmail.com>
2021-07-31 08:04:01 +0100

    Initial Commit

--- flat-file-generator.php
+++ flat-file-generator.php
@@ -9,33 +9,47 @@ Author URI: https://chrisbrosnan.me
 Text Domain: flat-file-generator
 */
 
-// Set Constants
-
-define('FFG_PATH', plugin_dir_path( __FILE__ ));
-
-class FlatFileGenerator
+function addMenu()
 {
 
-    function runPlugin()
-    {
-
-        // Include Core File
-
-        include( FFG_PATH . 'classes/Core.php');
-
-        $core = new Core;
-
-        $core->activate($path); 
-
-    }
+    add_menu_page( 
+        'Flat File Generator Options', 
+        'Flat File Generator', 
+        'manage_options', 
+        'flat-file-generator', 
+        'flatfilegeneratorUi'
+    );
 
 }
+add_action('admin_menu', 'addMenu');
 
-function runPlugin()
-{
-    $path = FFG_PATH;
-    $ffg = new FlatFileGenerator;
-    $ffg->runPlugin($path); 
-}
+// // Set Constants
 
-runPlugin();
+// define('FFG_PATH', plugin_dir_path( __FILE__ ));
+
+// class FlatFileGenerator
+// {
+
+//     function runPlugin()
+//     {
+
+//         // Include Core File
+
+//         include( FFG_PATH . 'classes/Core.php');
+
+//         $core = new Core;
+
+//         $core->activate($path); 
+
+//     }
+
+// }
+
+// function runPlugin()
+// {
+//     $path = FFG_PATH;
+//     $ffg = new FlatFileGenerator;
+//     $ffg->runPlugin($path); 
+// }
+
+// runPlugin();

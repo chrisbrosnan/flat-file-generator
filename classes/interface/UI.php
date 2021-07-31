2ff76db4a7c5d61ef7302eda750f0e9721bd2ac1
Chris Brosnan <cbrosnan00@gmail.com>
2021-07-31 08:03:30 +0100

    Initial Commit

--- classes/interface/UI.php
+++ classes/interface/UI.php
@@ -18,6 +18,20 @@ class UI
 
 }
 
+function addMenu()
+{
+
+    add_menu_page( 
+        'Flat File Generator Options', 
+        'Flat File Generator', 
+        'manage_options', 
+        'flat-file-generator', 
+        'flatfilegeneratorUi'
+    );
+
+}
+add_action('admin_menu', 'addMenu');
+
 function flatfilegeneratorUi()
 {
 

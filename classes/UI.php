c0d11f9455bf0f74b7a754e169f7e23cf6fbf087
Chris Brosnan <cbrosnan00@gmail.com>
2021-08-01 10:44:36 +0100

    UI

--- classes/UI.php
+++ classes/UI.php
@@ -42,10 +42,10 @@ class UI
             $o .= '<br/>'; 
             $o .= '<select name="content-type" style="width: 200px;">';
             $o .= '<option value="none">Select a data type</option>';
-                    foreach($data->dataTypes as $key => $value)
-                    {
-                        $o .= '<option value="'.$value.'">'.$key.'</option>'; 
-                    }
+                    // foreach($data->dataTypes as $key => $value)
+                    // {
+                    //     $o .= '<option value="'.$value.'">'.$key.'</option>'; 
+                    // }
             $o .= '</select>';
             $o .= '<br/>'; 
             $o .= '<input style="margin-top: 1em;" type="submit" value="Export">';

62e60fba9a23af6dc6ca9b26a638a57ad44ce5da
Chris Brosnan <cbrosnan00@gmail.com>
2021-08-01 11:36:07 +0100

    UI

--- flat-file-generator.php
+++ flat-file-generator.php
@@ -36,10 +36,47 @@ add_action('admin_menu', 'addMenu');
 
 function flatfilegeneratorUI()
 {
-    $dataTypes = new Data; 
-    $fileFormats = new Formats; 
-    $exportFormats = $fileFormats->exportFormats();
-    $dataTypesArray = $dataTypes->getDataTypes(); 
-    $ui = new UI; 
-    $ui->optionsLayout($exportFormats, $dataTypesArray); 
+    // $dataTypes = new Data; 
+    // $fileFormats = new Formats; 
+    // $exportFormats = $fileFormats->exportFormats();
+    // $dataTypesArray = $dataTypes->getDataTypes(); 
+    // $ui = new UI; 
+    // $ui->optionsLayout($exportFormats, $dataTypesArray); 
+?>
+// check user capabilities
+  if ( ! current_user_can( 'manage_options' ) ) {
+    return;
+  }
+
+  //Get the active tab from the $_GET param
+  $default_tab = null;
+  $tab = isset($_GET['tab']) ? $_GET['tab'] : $default_tab;
+
+  ?>
+  <!-- Our admin page content should all be inside .wrap -->
+  <div class="wrap">
+    <!-- Print the page title -->
+    <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+    <!-- Here are our tabs -->
+    <nav class="nav-tab-wrapper">
+      <a href="?page=my-plugin" class="nav-tab <?php if($tab===null):?>nav-tab-active<?php endif; ?>">Default Tab</a>
+      <a href="?page=my-plugin&tab=settings" class="nav-tab <?php if($tab==='settings'):?>nav-tab-active<?php endif; ?>">Settings</a>
+      <a href="?page=my-plugin&tab=tools" class="nav-tab <?php if($tab==='tools'):?>nav-tab-active<?php endif; ?>">Tools</a>
+    </nav>
+
+    <div class="tab-content">
+    <?php switch($tab) :
+      case 'settings':
+        echo 'Settings'; //Put your HTML here
+        break;
+      case 'tools':
+        echo 'Tools';
+        break;
+      default:
+        echo 'Default tab';
+        break;
+    endswitch; ?>
+    </div>
+  </div>
+<?php
 }
